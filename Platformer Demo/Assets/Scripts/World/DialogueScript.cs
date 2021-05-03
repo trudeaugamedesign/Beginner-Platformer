8bd05345da7caccdec838b04bb6757ca563716ae
luhyphenwai <luwaiwong@gmail.com>
2021-05-03 18:23:55 -0400

    almost done

--- Platformer Demo/Assets/Scripts/World/DialogueScript.cs
+++ Platformer Demo/Assets/Scripts/World/DialogueScript.cs
@@ -6,6 +6,7 @@ public class DialogueScript : MonoBehaviour
 {
     [Header("References")]
     public DialogueManager dm;
+    private Animator anim;
 
     [Header("Dialogue Settings")]
     [TextArea(3, 5)]
@@ -16,25 +17,29 @@ public class DialogueScript : MonoBehaviour
     void Start()
     {
         dm = GameObject.Find("Dialogue Manager").GetComponent<DialogueManager>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !dm.displayingText){
+        if (Input.GetKeyDown(KeyCode.E) && !dm.displayingText && playerInRange){
             dm.dialogue = dialogue;
             StartCoroutine(dm.StartDialogue());
+            anim.SetBool("Interactable", false);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player"){
             playerInRange = true;
+            anim.SetBool("Interactable", true);
         }
     }   
     private void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Player"){
             playerInRange = false;
+            anim.SetBool("Interactable", false);
         }
     }
 }
